75d508ff20ad3fe81dc270f3386b6fb793147889
David Lebsack <dlebsack@conquer.io>
2023-09-05 19:27:07 -0700

    avigation and colors when running in tmux

--- nvim/lua/custom/opts.lua
+++ nvim/lua/custom/opts.lua
@@ -43,8 +43,8 @@ vim.o.shiftwidth = 4
 -- vim.o.maxmempattern = 5000
 
 -- Set termcolors for tmux
---[[ if exists('+termguicolors')
-    let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
-    let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-    set termguicolors
-endif ]]
+-- if exists('+termguicolors')
+--     let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+--     let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+--     set termguicolors
+-- endif
