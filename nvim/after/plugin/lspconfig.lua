66ebab55630bb0ccda61aae881de9b8ea60694a3
David Lebsack <dlebsack@conquer.io>
2023-10-13 10:39:24 -0500

    fix conflicting commands ws for workspace symbols conflicted with worktree search, C-k signature help conflicted with navigation move up pane

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -23,11 +23,11 @@ local on_attach = function(_, bufnr)
 	nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
 	nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
 	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+	-- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
 	-- See `:help K` for why this keymap
 	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+	nmap('<leader>k', vim.lsp.buf.signature_help, 'Signature Documentation')
 
 	-- Lesser used LSP functionality
 	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
@@ -83,6 +83,10 @@ local servers = {
 			workspace = { checkThirdParty = false },
 			telemetry = { enable = false },
 		},
+	},
+	apex_ls = {
+		filetypes = { "apex" },
+		cmd = { 'java', '-jar', '/Users/lebsack/Downloads/apex-jorje-lsp.jar' },
 	},
 }
 -- Setup neovim lua configuration
