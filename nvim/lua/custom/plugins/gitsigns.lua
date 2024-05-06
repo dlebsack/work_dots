80ed86d95a75f9b1b875a6012758c8aa99e4cada
David Lebsack <dlebsack@conquer.io>
2024-05-06 15:02:52 -0500

    adjust gitsigns go to hunk keybinds

--- nvim/lua/custom/plugins/gitsigns.lua
+++ nvim/lua/custom/plugins/gitsigns.lua
@@ -1,4 +1,4 @@
-return  {
+return {
 	-- Adds git related signs to the gutter, as well as utilities for managing changes
 	'lewis6991/gitsigns.nvim',
 	opts = {
@@ -8,12 +8,16 @@ return  {
 			change = { text = '~' },
 			delete = { text = '_' },
 			topdelete = { text = '‾' },
-		changedelete = { text = '~' },
+			changedelete = { text = '~' },
 		},
+		current_line_blame = true,
 		on_attach = function(bufnr)
-			vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-			vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-			vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+			vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+				{ buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+			vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
+				{ buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+			vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk_inline,
+				{ buffer = bufnr, desc = '[P]review [H]unk' })
 		end,
 	},
 }
